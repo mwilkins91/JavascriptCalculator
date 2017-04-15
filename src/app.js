1670ada82da7f4a78c5d646e4a2f60d1c360c668
Mark Wilkins <markwilkins3@hotmail.com>
2017-04-15 15:01:02 -0400

    basic number inputs hooked up

--- src/app.js
+++ src/app.js
@@ -1,2 +1,16 @@
 import $ from 'jquery';
 
+$('.numBtn').on('click', (event) => {
+	event.preventDefault();
+	if($('.answer').text() == 0) {
+		$('.answer').text($(event.target).text());
+	}
+	else {
+		let previousText = $('.answer').text();
+		let newText = $('.answer').text() + $(event.target).text();
+		$('.answer').text(newText);
+	}
+});
+
+
+
