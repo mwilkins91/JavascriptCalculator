57759a8cadfbf99197c6b1d50cc42664d47fc0de
Mark Wilkins <markwilkins3@hotmail.com>
2017-04-16 01:42:35 -0400

    added operator functionality

--- src/app.js
+++ src/app.js
@@ -1,16 +1,69 @@
 import $ from 'jquery';
 
+const myApp = {};
+
+myApp.decimalAdded = false;
+myApp.freshCalc = true;
+
 $('.numBtn').on('click', (event) => {
 	event.preventDefault();
-	if($('.answer').text() == 0) {
+	//If the answer should be overwritten...
+	if (myApp.freshCalc && !myApp.decimalAdded) {
 		$('.answer').text($(event.target).text());
-	}
-	else {
+		myApp.freshCalc = false;
+	} else if (($('.answer').text() === '0' && $(event.target).text() === '0')) {
+		$('.answer').text($(event.target).text());
+		myApp.freshCalc = true;
+	} else {
 		let previousText = $('.answer').text();
 		let newText = $('.answer').text() + $(event.target).text();
 		$('.answer').text(newText);
+		myApp.freshCalc = false;
 	}
 });
 
+$('.decimal').on('click', function(event) {
+	event.preventDefault();
+	/* Act on the event */
 
+	if (myApp.freshCalc) {
+		$('.answer').text('0.');
+		myApp.freshCalc = false;
+	} else if (!myApp.decimalAdded) {
+		let previousText = $('.answer').text();
+		let newText = $('.answer').text() + $(event.target).text();
+		$('.answer').text(newText);
+		myApp.decimalAdded = true;
+	}
+});
 
+$('.operator').on('click', function(event) {
+	event.preventDefault();
+	/* Act on the event */
+	if (!myApp.freshCalc) {
+		if ($('.calcChain').text() === '') {
+			$('.calcChain').text($('.answer').text() + ' ' + $(event.target).text());
+		} else {
+			$('.calcChain').text($('.calcChain').text() + ' ' + $('.answer').text() + ' ' + $(event.target).text());
+		}
+	} else {
+		let calcChainLength = $('.calcChain').text().length;
+		if(calcChainLength > 0) {
+			let currentCalcChain = $('.calcChain').text();
+			let shatteredChain = currentCalcChain.split('');
+			shatteredChain[shatteredChain.length - 1] = $(event.target).text();
+			let newJoinedChain = shatteredChain.join('');
+			$('.calcChain').text(newJoinedChain);
+		}
+	}
+
+	myApp.freshCalc = true;
+});
+
+$('.clear').on('click', function(event) {
+	event.preventDefault();
+	/* Act on the event */
+	$('.answer').text(0);
+	myApp.decimalAdded = false;
+	myApp.freshCalc = true;
+});
